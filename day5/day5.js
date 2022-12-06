92bcb1f47a9585d2abf9d0477c8b1d48106bd374
Melwin Kramer <melwin.kramer@gmail.com>
2022-12-06 19:02:30 +0100

    Day5 JavaScript version

---
+++ day5/day5.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+let stacks = [
+    ['Q', 'W', 'P', 'S', 'Z', 'R', 'H', 'D'],
+    ['V','B','R','W','Q','H','F'],
+    ['C','V','S','H'],
+    ['H','F','G'],
+    ['P','G','J','B','Z'],
+    ['Q','T','J','H','W','F','L'],
+    ['Z','T','W','D','L','V','J','N'],
+    ['D','T','Z','C','J','G','H','F'],
+    ['W','P','V','M','B','H'],
+]
+
+const input = fs.readFileSync("input.txt", "utf-8");
+const operations = input.split('\n');
+
+operations.forEach(operation => {
+    let para = operation.match(/\d+/g);
+    let removedElement = []
+    for(var i = 0; i < para[0]; i++) {
+        removedElement.push(stacks[para[1]-1].pop());
+    }
+    removedElement.reverse();
+    for(e of removedElement) stacks[para[2]-1].push(e);
+});
+
+for(stack of stacks) console.log(stack[stack.length - 1])
